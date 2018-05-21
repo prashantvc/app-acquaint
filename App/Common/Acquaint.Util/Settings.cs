ab7bcc1b1267b0aed4937e708da8303c7f0a399f
Prashant Cholachagudda <prchol@microsoft.com>
2018-05-21 17:13:35 +0530

    Update nuget packages

--- App/Common/Acquaint.Util/Settings.cs
+++ App/Common/Acquaint.Util/Settings.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using Plugin.Settings;
-using Plugin.Settings.Abstractions;
+using Xamarin.Essentials;
 
 namespace Acquaint.Util
 {
@@ -9,10 +8,7 @@ namespace Acquaint.Util
 	/// </summary>
 	public static class Settings
 	{
-		private static ISettings AppSettings
-		{
-			get { return CrossSettings.Current; }
-		}
+
 
 		public static void ResetUserConfigurableSettingsToDefaults()
 		{
@@ -40,35 +36,35 @@ namespace Acquaint.Util
 
 		#region user-configurable
 
-		private const string AzureAppServiceUrlKey = "AzureAppServiceUrl_key";
-		private static readonly string AzureAppServiceUrlDefault = "https://app-acquaint.azurewebsites.net";
+		const string AzureAppServiceUrlKey = "AzureAppServiceUrl_key";
+		static readonly string AzureAppServiceUrlDefault = "https://app-acquaint.azurewebsites.net";
 
-		private const string DataPartitionPhraseKey = "DataPartitionPhrase_key";
-		private static readonly string DataSeedPhraseDefault = "";
+		const string DataPartitionPhraseKey = "DataPartitionPhrase_key";
+		static readonly string DataSeedPhraseDefault = "";
 
-		private const string ImageCacheDurationHoursKey = "ImageCacheDurationHours_key";
+		const string ImageCacheDurationHoursKey = "ImageCacheDurationHours_key";
 		public static readonly int ImageCacheDurationHoursDefault = 1;
 
 		public static string AzureAppServiceUrl
 		{
-			get { return AppSettings.GetValueOrDefault<string>(AzureAppServiceUrlKey, AzureAppServiceUrlDefault); }
-			set { AppSettings.AddOrUpdateValue<string>(AzureAppServiceUrlKey, value); }
+			get => Preferences.Get(AzureAppServiceUrlKey, AzureAppServiceUrlDefault);
+			set => Preferences.Set(AzureAppServiceUrlKey, value);
 		}
 
 		public static string DataPartitionPhrase
 		{
-			get { return AppSettings.GetValueOrDefault<string>(DataPartitionPhraseKey, DataSeedPhraseDefault); }
+			get => Preferences.Get(DataPartitionPhraseKey, DataSeedPhraseDefault);
 			set
-			{ 
-				AppSettings.AddOrUpdateValue<string>(DataPartitionPhraseKey, value);
+			{
+				Preferences.Set(DataPartitionPhraseKey, value);
 				RaiseDataParitionPhraseChangedEvent(null);
 			}
 		}
 
 		public static int ImageCacheDurationHours
 		{
-			get { return AppSettings.GetValueOrDefault<int>(ImageCacheDurationHoursKey, ImageCacheDurationHoursDefault); }
-			set { AppSettings.AddOrUpdateValue<int>(ImageCacheDurationHoursKey, value); }
+			get => Preferences.Get(ImageCacheDurationHoursKey, ImageCacheDurationHoursDefault);
+			set => Preferences.Set(ImageCacheDurationHoursKey, value);
 		}
 
 		#endregion
@@ -88,38 +84,38 @@ namespace Acquaint.Util
 		private const string HockeyAppIdKey = "HockeyAppId_key";
 		private static readonly string HockeyAppIdDefault = "11111111222222223333333344444444"; // This is just a placeholder value. Replace with your real HockeyApp App ID.
 
-        private const string BingMapsKeyKey = "BingMapsKey_key";
-        private static readonly string BingMapsKeyDefault = "UW0peICp3gljJyhqQKFZ~R3XF1I5BvWmWmkD4ujytTA~AoUOqpk2nJB-Wh7wH-9S-zaG-w6sygLitXugNOqm71wx_nc6WHIt6Lb29gyTU04X";
+		private const string BingMapsKeyKey = "BingMapsKey_key";
+		private static readonly string BingMapsKeyDefault = "UW0peICp3gljJyhqQKFZ~R3XF1I5BvWmWmkD4ujytTA~AoUOqpk2nJB-Wh7wH-9S-zaG-w6sygLitXugNOqm71wx_nc6WHIt6Lb29gyTU04X";
 
 		public static bool LocalDataResetIsRequested
 		{
-			get { return AppSettings.GetValueOrDefault<bool>(LocalDataResetIsRequestedKey, LocalDataResetIsRequestedDefault); }
-			set { AppSettings.AddOrUpdateValue<bool>(LocalDataResetIsRequestedKey, value); }
+			get => Preferences.Get(LocalDataResetIsRequestedKey, LocalDataResetIsRequestedDefault);
+			set => Preferences.Set(LocalDataResetIsRequestedKey, value);
 		}
 
 		public static bool ClearImageCacheIsRequested
 		{
-			get { return AppSettings.GetValueOrDefault<bool>(ClearImageCacheIsRequestedKey, ClearImageCacheIsRequestedDefault); }
-			set { AppSettings.AddOrUpdateValue<bool>(ClearImageCacheIsRequestedKey, value); }
+			get => Preferences.Get(ClearImageCacheIsRequestedKey, ClearImageCacheIsRequestedDefault);
+			set => Preferences.Set(ClearImageCacheIsRequestedKey, value);
 		}
-
-        public static bool DataIsSeeded
+        
+		public static bool DataIsSeeded
 		{
-			get { return AppSettings.GetValueOrDefault<bool>(DataIsSeededKey, DataIsSeededDefault); }
-			set { AppSettings.AddOrUpdateValue<bool>(DataIsSeededKey, value); }
+			get => Preferences.Get(DataIsSeededKey, DataIsSeededDefault);
+			set => Preferences.Set(DataIsSeededKey, value);
 		}
 
 		public static string HockeyAppId
 		{
-			get { return AppSettings.GetValueOrDefault<string>(HockeyAppIdKey, HockeyAppIdDefault); }
-			set { AppSettings.AddOrUpdateValue<string>(HockeyAppIdKey, value); }
+			get => Preferences.Get(HockeyAppIdKey, HockeyAppIdDefault);
+			set => Preferences.Set(HockeyAppIdKey, value);
 		}
-
-        public static string BingMapsKey
-        {
-            get { return AppSettings.GetValueOrDefault<string>(BingMapsKeyKey, BingMapsKeyDefault); }
-            set { AppSettings.AddOrUpdateValue<string>(BingMapsKeyKey, value); }
-        }
+        
+		public static string BingMapsKey
+		{
+			get => Preferences.Get(BingMapsKeyKey, BingMapsKeyDefault);
+			set => Preferences.Set(BingMapsKeyKey, value);
+		}
 
 		#endregion
 	}
